64f5e57c0c21af2b38dd78721f48788552d9d348
Edison Chica <edisonc9@outlook.com>
2023-08-16 20:28:56 -0500

    Se fusiona con mobile menu y se hace mediaquery

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
 const desktopMenu = document.querySelector(".desktop-menu");
 const navmail = document.querySelector(".navbar-email");
-const arrow = document.querySelector(".arrow")
+const arrow = document.querySelector(".arrow");
+const burgerMenu = document.querySelector(".menu");
+const mobileMenu = document.querySelector(".mobile-menu");
 
 navmail.addEventListener("click", toggleDesktopMenu);
 arrow.addEventListener("click", toggleDesktopMenu);
+burgerMenu.addEventListener("click", togglemobileMenu);
 
 function toggleDesktopMenu(){
-    desktopMenu.classList.toggle("inactive")
+    desktopMenu.classList.toggle("inactive");
+}
+
+function togglemobileMenu(){
+     mobileMenu.classList.toggle("inactive");
 }
